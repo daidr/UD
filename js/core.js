581c468655f7e81df748481d9a0437b08ade6bc7
戴兜 <daidou@daidou.pw>
2017-05-23 22:30:27 +0800

    mobile

--- js/core.js
+++ js/core.js
@@ -3,6 +3,15 @@ $(function() {
 	setTimeout("$('.loading').slideUp('slow');", 5500);
 	setTimeout("$('.loading').remove();", 10000);
 	$(".header").load("page/header." + language("get") + ".ud");
-	$(".slide").load("page/index.slide.cn.ud");
+	$(".slide").load("page/index.slide." + language("get") + ".ud");
 	$(".projects").load("page/index.projects." + language("get") + ".ud");
+});
+
+$(window).scroll(function(){
+var s=$(window).scrollTop();
+if(s>20){
+	$(".navbar").css("background", "#686967");
+} else {
+	$(".navbar").css("background", "transparent");
+}
 });
